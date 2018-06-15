92ea8e4959a28cba160b75eae09ab422f39d4124
Aitor González Fernández <info@aitorgf.com>
2018-06-15 22:34:46 +0200

    Added the definition of router, the root of a Zone tree

---
+++ network/kad/router/router.go
@@ -0,0 +1,54 @@
+package router
+
+import (
+	"github.com/tokkenno/sleepy/types"
+	"errors"
+	"time"
+	"github.com/tokkenno/sleepy/utils/event"
+	"math/rand"
+)
+
+// The router is the special zone in the root of a zone tree
+type Router struct {
+	Zone
+	randomGenerator        *rand.Rand
+	peerUpdateRequestEvent *event.Emitter
+}
+
+// Load a router zone tree from file
+func LoadRouterFromFile(localId types.UInt128, path string) (*Router, error) {
+	return nil, errors.New("not implemented yet")
+}
+
+// Create a new Zone tree (Router) from the local peer Id
+func NewRouter(id types.UInt128) *Router {
+	rz := &Router{
+		Zone: Zone{
+			localId:           id,
+			zoneIndex:         *types.NewUInt128FromInt(0),
+			parent:            nil,
+			leftChild:         nil,
+			rightChild:        nil,
+			level:             0,
+			bucket:            new(kBucket),
+			randomLookupTimer: nil,
+		},
+		randomGenerator:        rand.New(rand.NewSource(time.Now().UnixNano())),
+		peerUpdateRequestEvent: event.NewEvent(),
+	}
+
+	rz.Zone.root = rz
+
+	rz.startChecks()
+
+	return rz
+}
+
+// Event fired when a peer need update his information
+func (router *Router) PeerUpdateRequestEvent() *event.Handler {
+	return router.peerUpdateRequestEvent.GetHandler()
+}
+
+func (router *Router) SaveFile(path string) error {
+	return errors.New("not implemented yet")
+}
